f17faa8fd820cc943e44a45632de8e79b4462c53
Jethro Roy <jethroroy@gmail.com>
2025-09-22 14:38:30 -0400

    Add basic swagger ui

--- src/app.py
+++ src/app.py
@@ -2,21 +2,32 @@
 #Run app.py localy and visit localhost to navigate or test the API
 #Use: pip install flask flask-swagger-ui requests pytest
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 import requests
 import os
 from dotenv import load_dotenv
+from flask_swagger_ui import get_swaggerui_blueprint
 
 load_dotenv() 
 app = Flask(__name__)
 
-# set le token dans une variable locale
-
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-
-API_URL = f"https://api.github.com/repos/cjayneb/metrics-a25-grp1-eq10"
-
+GITHUB_API_URL = "https://api.github.com/repos/cjayneb/metrics-a25-grp1-eq10"
 headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+
+SWAGGER_URL = '/docs'
+SWAGGER_API_URL = '/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    SWAGGER_API_URL,
+    config={'app_name': "Kanban Metrics API"}
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/swagger.yaml')
+def swagger_yaml():
+    return send_from_directory('.', 'swagger.yaml')
 
 @app.route("/")
 def home():
@@ -24,8 +35,7 @@ def home():
 
 @app.route("/issues", methods=["GET"])
 def get_issues():
-    """Return issues of the repo"""
-    r = requests.get(f"{API_URL}/issues?state=all", headers=headers)
+    r = requests.get(f"{GITHUB_API_URL}/issues?state=all", headers=headers)
     return jsonify(r.json())
 
 if __name__ == "__main__":
